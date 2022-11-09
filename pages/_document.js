62a99b5da81169fd0f01c758c1d00da26d0be110
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-09 08:35:53 -0800

    intialk

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
     return (
             <Html>
                 <Head />
+
                 <title>{process.env.NEXT_PUBLIC_PROPERTY_ID}</title>
                 <body className={"dark:bg-black dark:text-white"}>
                     <Main />
--- public/components/fab.js
+++ public/components/fab.js
@@ -6,6 +6,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
 
+
 export default function FloatingActionButtons() {
     return (
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
