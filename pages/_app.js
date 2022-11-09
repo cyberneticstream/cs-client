daaff40f548c097ee1f0832ca758e4d99feb937c
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-09 11:23:55 -0800

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "map.css"
 import "./styles/buildtailwind.css"
+import Script from "next/script";
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+            <>
+            <Component {...pageProps} />
+            <Script src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js" crossorigin async data-callback="initMapKit" data-libraries="map,annotations,services" data-initial-token="" lazyOnLoad={true}></Script>
+            </>
+            )
 }
