11b39060da0ad035b5e13901cee868513b6df59e
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-09 14:18:01 -0800

    d

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,6 @@ export default function MyApp({ Component, pageProps }) {
             <>
                 <Component {...pageProps} />
             <Script src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.core.js" crossorigin async data-callback="initMapKit" data-libraries="map,annotations,services" data-initial-token="" lazyOnLoad={true}></Script>
-                </>g
+                
             )
 }
